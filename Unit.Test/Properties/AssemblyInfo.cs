a382e70d420a164cc4cd1a5c889360dbf96f1e93
Paul Nokes <paul.nokes@health.wa.gov.au>
2024-04-03 13:06:07 +0800

    Add project files.

---
+++ Unit.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WA Health")]
+[assembly: AssemblyProduct("Unit.Test")]
+[assembly: AssemblyCopyright("Copyright © WA Health 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aaac82cb-4f57-45a8-901a-957bfa5bf916")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Unit.Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace Unit.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Initialise()
+        {
+            IServiceCollection services = new ServiceCollection()
+                .AddSingleton<ILogger, Logger>();
+
+            ServiceProvider Provider = services.BuildServiceProvider();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    public interface ILogger
+    {
+    }
+
+    public class Logger : ILogger
+    {
+    }
+}
